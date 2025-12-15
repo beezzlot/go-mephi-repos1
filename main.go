f30c221d3036aa6bd90189ac134c510af4924078
Mikhail Mokshantsev <m.mokshantsev@reglab.ru>
2025-12-15 18:39:03 +0500

    Fix for go x2

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 )
 
 func main() {
-	errors := 0
-	addr := "http://srv.msk01.gigacorp.local/_stats"
+	fails := 0
+	host := "http://srv.msk01.gigacorp.local/_stats"
 
 	for {
-		resp, err := http.Get(addr)
+		resp, err := http.Get(host)
 		if err != nil {
-			errors++
-			if errors >= 3 {
+			fails++
+			if fails >= 3 {
 				fmt.Println("Unable to fetch server statistic")
 			}
 			time.Sleep(10 * time.Second)
@@ -24,86 +24,85 @@ func main() {
 		}
 
 		if resp.StatusCode != 200 {
-			errors++
+			fails++
 			resp.Body.Close()
-			if errors >= 3 {
+			if fails >= 3 {
 				fmt.Println("Unable to fetch server statistic")
 			}
 			time.Sleep(10 * time.Second)
 			continue
 		}
 
-		buf := make([]byte, 256)
-		n, err := resp.Body.Read(buf)
+		data := make([]byte, 512)
+		n, err := resp.Body.Read(data)
 		resp.Body.Close()
 
-		if n == 0 || err != nil {
-			errors++
-			if errors >= 3 {
+		if n == 0 {
+			fails++
+			if fails >= 3 {
 				fmt.Println("Unable to fetch server statistic")
 			}
 			time.Sleep(10 * time.Second)
 			continue
 		}
 
-		text := strings.TrimSpace(string(buf[:n]))
-		items := strings.Split(text, ",")
+		line := strings.TrimSpace(string(data[:n]))
+		parts := strings.Split(line, ",")
 
-		if len(items) != 6 {
-			errors++
-			if errors >= 3 {
+		if len(parts) != 7 {
+			fails++
+			if fails >= 3 {
 				fmt.Println("Unable to fetch server statistic")
 			}
 			time.Sleep(10 * time.Second)
 			continue
 		}
 
-		var vals [6]float64
-		ok := true
-		for i, s := range items {
-			v, e := strconv.ParseFloat(strings.TrimSpace(s), 64)
+		var nums [7]float64
+		good := true
+		for i, p := range parts {
+			v, e := strconv.ParseFloat(strings.TrimSpace(p), 64)
 			if e != nil {
-				ok = false
+				good = false
 				break
 			}
-			vals[i] = v
+			nums[i] = v
 		}
 
-		if !ok {
-			errors++
-			if errors >= 3 {
+		if !good {
+			fails++
+			if fails >= 3 {
 				fmt.Println("Unable to fetch server statistic")
 			}
 			time.Sleep(10 * time.Second)
 			continue
 		}
 
-		errors = 0
+		fails = 0
 
-		if vals[0] > 30 {
-			fmt.Printf("Load Average is too high: %v\n", vals[0])
+		if nums[0] > 30 {
+			fmt.Printf("Load Average is too high: %v\n", nums[0])
 		}
 
-		if vals[1] > 0 {
-			memPercent := (vals[2] / vals[1]) * 100
-			if memPercent > 80 {
-				fmt.Printf("Memory usage too high: %v%%\n", memPercent)
+		if nums[1] > 0 {
+			memPct := (nums[2] / nums[1]) * 100
+			if memPct > 80 {
+				fmt.Printf("Memory usage too high: %v%%\n", memPct)
 			}
 		}
 
-		if vals[3] > 0 {
-			diskPercent := (vals[4] / vals[3]) * 100
-			if diskPercent > 90 {
-				freeMb := (vals[3] - vals[4]) / (1024 * 1024)
+		if nums[3] > 0 {
+			diskPct := (nums[4] / nums[3]) * 100
+			if diskPct > 90 {
+				freeMb := (nums[3] - nums[4]) / (1024 * 1024)
 				fmt.Printf("Free disk space is too low: %v Mb left\n", freeMb)
 			}
 		}
 
-		bandwidth := 6551603348.0
-		if bandwidth > 0 {
-			netPercent := (vals[5] / bandwidth) * 100
-			if netPercent > 90 {
-				freeMbit := (bandwidth - vals[5]) * 8 / (1024 * 1024)
+		if nums[5] > 0 {
+			netPct := (nums[6] / nums[5]) * 100
+			if netPct > 90 {
+				freeMbit := (nums[5] - nums[6]) * 8 / (1024 * 1024)
 				fmt.Printf("Network bandwidth usage high: %v Mbit/s available\n", freeMbit)
 			}
 		}
